af8c896974dafd5abccf9e195018dba8de66fb1f
Alex Bird <alex.bird@hunterindustries.com>
2025-01-15 07:54:20 -0800

    Update diyUnit to use clear test names in the output

--- diyUnit.mjs
+++ diyUnit.mjs
@@ -2,10 +2,14 @@ global.describe = (describeName, describeBody) => {
 
   global.it = (testName, testBody) => {
 
+    const fullName = `${describeName} > ${testName}`;
+
     global.expect = (actual) => {
       return {
         toEqual: (expected) => {
-          console.log({ result: actual === expected ? '✅ PASS' : '❌ FAIL', actual, expected });
+          const pass = actual === expected;
+          const icon = pass ? '✅' : '❌';
+          console.log(`${icon} ${fullName}`);
         }
       }
     }
