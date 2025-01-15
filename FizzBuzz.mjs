392bbbdf4b8ab7f58ce31f2d4c92cb078d762c23
Alex Bird <alex.bird@hunterindustries.com>
2025-01-15 08:12:07 -0800

    Re-enable the commented out code to get the tests to pass

--- FizzBuzz.mjs
+++ FizzBuzz.mjs
@@ -1,9 +1,9 @@
 export function toFizzBuzzString(index) {
-  //if (index === 5) {
-    //return "Buzz";
-  //}
-  //if (index === 3) {
-    //return "Fizz";
-  //}
+  if (index === 5) {
+    return "Buzz";
+  }
+  if (index === 3) {
+    return "Fizz";
+  }
   return index;
 }
